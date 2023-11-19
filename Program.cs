5e846c18f823a8aedf8d1e495a7d09cda0968d54
yuhsi <yuhsi@PC1>
2023-11-19 16:48:16 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("請輸入您的名字：");
+        string userName = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine($"歡迎, {userName}!");
+        }
+        else
+        {
+            Console.WriteLine("您未輸入名字。");
+        }
+    }
+}
